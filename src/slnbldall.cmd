ad12dcd7523ea3283a02e3717ad811245eaa280c
Kelvin Lee <kiyolee@gmail.com>
2019-05-30 10:15:38 +1000

    Add slnbldall.cmd that builds all common configurations of a solution.

---
+++ src/slnbldall.cmd
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+if .%1. == .. goto :end
+set SLN=%1
+
+if .%2. == .. goto :notarget
+set TARGET=%2
+goto :nexttarget
+:notarget
+set TARGET=Build
+:nexttarget
+
+for %%p in ( Win32 x64 ) do (
+for %%c in ( Release Debug ) do (
+  msbuild %SLN% /m /t:%TARGET% /p:Platform=%%p /p:Configuration=%%c
+)
+)
+
+:end
+endlocal
