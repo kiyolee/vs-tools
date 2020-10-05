bd3d3e8b0ef6327dc331f26f7c8f5c6ba31ff5af
Kelvin Lee <kiyolee@gmail.com>
2020-10-05 13:06:48 +1100

    Add commands to set env for various versions of VS.

---
+++ src/vs2017.cmd
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+set _VSVER_=VS2017
+set _GCT_=
+set _CONTITLE_=
+for %%i in ( get_console_title.cmd ) do set "_GCT_=%%~$PATH:i"
+if "%_GCT_%" == "" goto :notitle
+for /f "delims=" %%i in ('get_console_title.cmd "%~n0"') do set "_CONTITLE_=%%i"
+:notitle
+if "%_CONTITLE_%" == "" goto :deftitle
+set _CONTITLE_=%_CONTITLE_% - %_VSVER_%
+goto :dovc
+:deftitle
+set _CONTITLE_=%_VSVER_%
+:dovc
+title %_CONTITLE_%
+endlocal
+pushd .
+call vcvarsreset.cmd
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat" %*
+popd
