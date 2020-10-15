bc42453860dacc8dba4423e6e3e58e7a85f9e928
Kelvin Lee <kiyolee@gmail.com>
2020-10-15 22:38:53 +1100

    Update VisualStudioVersion for 2017|2019.

--- src/newsln19.py
+++ src/newsln19.py
@@ -9,7 +9,7 @@ import os
 DEFAULT_SLN = r'''
 Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio Version 16
-VisualStudioVersion = 16.0.28803.156
+VisualStudioVersion = 16.0.30611.23
 MinimumVisualStudioVersion = 10.0.40219.1
 Global
 	GlobalSection(SolutionProperties) = preSolution
